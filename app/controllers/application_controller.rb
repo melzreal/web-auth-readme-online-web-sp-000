8377972c65fb14b369db9ec2bb987b5ee563cb41
Melissa Real <melissa.real@hotmail.com>
2019-05-29 22:23:02 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user
-  config.web_console.whitelisted_ips = '31.127.99.181'
-
+  
   private
 
     def authenticate_user
